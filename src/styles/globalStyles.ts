07f2abeda82d926501dec299f47dc55df1331f68
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2021-12-12 12:50:43 +0530

    Feat/qol changes (#7)
    * for code use the IBM plex mono font
    
    * remove params of item?id= to route links inside hckrnws

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -2,12 +2,20 @@ import { globalCss } from "@stitches/react";
 
 export const globalStyles = globalCss({
   "*": { margin: 0, padding: 0, boxSizing: "border-box" },
-  "@font-face": {
-    fontFamily: "IBM Plex Sans",
-    fontStyle: "normal",
-    fontWeight: " 100 900",
-    src: "url(/fonts/ibm-plex-sans-var.woff2) format('woff2')",
-  },
+  "@font-face": [
+    {
+      fontFamily: "IBM Plex Sans",
+      fontStyle: "normal",
+      fontWeight: " 100 900",
+      src: "url(/fonts/ibm-plex-sans-var.woff2) format('woff2')",
+    },
+    {
+      fontFamily: "IBM Plex Mono",
+      fontStyle: "normal",
+      fontWeight: " 100 900",
+      src: "url(/fonts/IBMPlexMono-Text.woff2) format('woff2')",
+    },
+  ],
   html: {
     minWidth: "360px",
     scrollBehavior: "smooth",
