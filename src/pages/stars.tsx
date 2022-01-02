01460af0ecb641cac7ea9895700b56af35edf5f9
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-01-02 14:42:03 +0530

    stars page created

---
+++ src/pages/stars.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from "next";
+import { TBaseStory } from "types/story";
+import StoryListItem from "@components/StoryListItem";
+import Head from "next/head";
+import { Fragment } from "react";
+import { styled } from "../../stitches.config";
+import useStore from "store/useStore";
+
+type PageProps = {
+  response: TBaseStory[];
+};
+
+const Box = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  marginTop: "48px",
+
+  "@tablet": {
+    marginTop: "24px",
+  },
+
+  "@phone": {
+    marginTop: "16px",
+  },
+});
+
+const Star: NextPage<PageProps> = (props: PageProps) => {
+  const starred = useStore((state) => state.starred);
+  return (
+    <Fragment>
+      <Head>
+        <title>Starred - hckrnws </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Box>
+        {starred.map((story) => (
+          <StoryListItem story={story} key={story.id} />
+        ))}
+      </Box>
+    </Fragment>
+  );
+};
+
+export default Star;
