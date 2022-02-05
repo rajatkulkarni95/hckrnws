7f877d91840d6bc72215bbf40e0ccce9abec5d35
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2022-02-05 08:26:41 +0530

    Installs preact dependency and changes in config for swapping (#15)

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 module.exports = {
     swcMinify: true,
     reactStrictMode: true,
-    webpack(config) {
+    webpack(config, { dev, isServer }) {
+        // Replace React with Preact only in client production build
+        if (!dev && !isServer) {
+            Object.assign(config.resolve.alias, {
+                react: "preact/compat",
+                "react-dom/test-utils": "preact/test-utils",
+                "react-dom": "preact/compat",
+            });
+        }
+
         config.module.rules.push({
             test: /\.svg$/,
             use: ["@svgr/webpack"],
